949ce692d54d88c4ac8d6da138e5f88d174d7e15
yadav-angad <yadavangad100@gmail.com>
2025-08-03 02:03:13 -0500

    Added gh-pages

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Box, Card, Button, Alert, TextField, Typography, TableContainer, TableHead, TableRow, TableCell, TableBody, Table } from "@mui/material";
+import { store } from 'host/store';
+import { useSelector, useDispatch } from 'react-redux';
+
+export default function () {
+  const [userName, setUserName] = React.useState('');
+  const countryName = 'United States'; // Example country name, can be dynamic
+  const dispatch = useDispatch();
+  store.subscribe(() => console.log(store.getState()));
+  const { countryData, user } = useSelector((state) => state);
+  const { country, populationCounts } = countryData[countryName] || {};
+
+  return (
+    <Card sx={{ width: '20%', height: 'calc(100vh - 140px)', backgroundColor: '#f5f5f5', padding: '10px' }}>
+      <Alert severity="info">{'MFE 2 getting data from Host'}</Alert>
+      <Box>
+        <TextField
+          label="Enter Name"
+          variant="outlined"
+          value={userName ?? ''}
+          sx={{ marginBottom: '10px', width: '100%' }}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <Button sx={{ backgroundColor: 'DarkGreen', color: 'white', '&:hover': { backgroundColor: 'Green' } }} onClick={() => {
+          dispatch({ type: 'SET_USER', payload: { name: userName } })
+        }}>
+          {'Set Username'}
+        </Button>
+        <Typography>{'Hello : ' + user?.name}</Typography>
+      </Box>
+      <Box>
+        {countryData[countryName] && (
+          <div className="united-states-population-container">
+            <h1>Population of {country}</h1>
+            <div className="population-cards-container">
+              <TableContainer sx={{ border: '1px solid #ccc', borderRadius: 2 }}>
+                <Table sx={{ minWidth: 300 }} size="small">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell sx={{ border: '1px solid #ccc', fontWeight: 'bold' }}>Year</TableCell>
+                      <TableCell sx={{ border: '1px solid #ccc', fontWeight: 'bold' }}>Population</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {[...populationCounts]
+                      .sort((a, b) => b.year - a.year)
+                      .slice(0, 3)
+                      .map((yearData) => (
+                        <TableRow key={yearData.year}>
+                          <TableCell sx={{ border: '1px solid #ccc' }}>{yearData.year}</TableCell>
+                          <TableCell sx={{ border: '1px solid #ccc' }}>
+                            {yearData.value.toLocaleString()}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </div>
+            <p className="source-info">Source: World Bank (or specify actual source)</p>
+          </div>
+        )}
+      </Box>
+    </Card>
+  );
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const dependencies = require("./package.json").dependencies;
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+  },
+  devServer: {
+    port: 3002,
+    liveReload: true,
+    historyApiFallback: true,
+  },
+  name: "mfe-usa",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new ModuleFederationPlugin({
+      name: "MfeUsa",
+      filename: "remoteEntry.js",
+      remotes: {
+        host: 'host@http://localhost:3000/remoteEntry.js',
+      },
+      exposes: {
+        "./MfeUsa": "./src/App.js"
+      },
+      shared: {
+        "react": {
+          singleton: true,
+          requiredVersion: dependencies.react
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: dependencies["react-dom"]
+        },
+        "@mui/material": {
+          singleton: true,
+          requiredVersion: dependencies["@mui/material"]
+        },
+        "@mui/icons-material": {
+          singleton: true,
+          requiredVersion: dependencies["@mui/icons-material"]
+        },
+        "redux": {
+          singleton: true,
+          requiredVersion: dependencies.redux
+        },
+        "react-redux": {
+          singleton: true,
+          requiredVersion: dependencies["react-redux"]
+        }
+      }
+    })
+  ],
+};
